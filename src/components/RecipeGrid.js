b5871135b396e2c61315c6a594889e71425f00e1
Kenny Kenn <krecoding@gmail.com>
2019-05-01 14:18:59 -0400

    init

---
+++ src/components/RecipeGrid.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+// Grid display of recipes after user requests a query
+class RecipeGrid extends Component {
+	constructor(props){
+        super(props)
+        console.log(props)
+    }
+    
+    render() {
+        return (
+            <div>
+                Recipe grid goes here :)
+            </div>
+        )
+    }
+}
+
+export default RecipeGrid;
