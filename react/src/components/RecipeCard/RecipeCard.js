ba8966cfba98d6131d158463902eb893f91d3b16
Kenny Kenn <krecoding@gmail.com>
2019-10-22 22:04:36 -0400

    WIP version

--- react/src/components/RecipeCard/RecipeCard.js
+++ react/src/components/RecipeCard/RecipeCard.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+import Media from 'react-bootstrap/Media';
+import Button from 'react-bootstrap/Button';
 
 // RecipeCard
 class RecipeCard extends Component {
@@ -12,10 +14,28 @@ class RecipeCard extends Component {
     
     render() {
         return (
-            <Row>
-                <Col xs={6}>
-                    <Image src={this.props.recipe.thumbnail} fluid rounded />
-                </Col>
+            <Row className="text-center wte-recipe-card p-3">
+                <Media>
+                    <img
+                    width="50%"
+                    height="100%"
+                    className="align-self-center mr-3"
+                    src={this.props.recipe.thumbnail}
+                    alt="Generic placeholder"
+                    id="wte-thumbnail"
+                    />
+                    <Media.Body>
+                    <h5>Recipe by</h5>
+                    <p>
+                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                        ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+                        tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                        fringilla. Donec lacinia congue felis in faucibus.
+                    </p>
+
+                    <Button variant="dark" href={this.props.recipe.href}>READ FULL RECIPE</Button>
+                    </Media.Body>
+                </Media>
             </Row>
         )
     }
